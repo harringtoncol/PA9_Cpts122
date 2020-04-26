5547d9fd81b52fc5af6b2d4acb5662f0c32cdbc1
Cole Harrington <harringtoncol17@gmail.com>
2020-04-25 20:58:56 -0700

    Oops forgot to push this

--- BasicZombie.h
+++ BasicZombie.h
@@ -5,12 +5,22 @@
 
 class BasicZombie : public GameObject {
 private:
-	double speed = 4;
+	double speed = 10;
 	double damage = 20;
 	double health = 100;
 	sf::Sprite zombieObj;
 	sf::Vector2f lastPostition;
-	sf::Texture texture;
+	sf::Texture textureN;
+	sf::Texture textureNE;
+	sf::Texture textureSE; 
+	sf::Texture textureSW;
+	sf::Texture textureS;
+	sf::Texture textureW;
+	sf::Texture textureNW;
+	sf::Texture textureE;
+	sf::Texture* texture;
+	int direction[8]; //N, NE, E, SE , S, NW,W, SW
+	int index;
 public:
 	BasicZombie();
 	~BasicZombie();
@@ -18,7 +28,7 @@ public:
 	void Update(sf::RenderWindow& Window) override;
 	void takeDamage(double);
 	int dealDamage();
-
+	
 
 
 
