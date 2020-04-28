5b7fb268370ebc6110f71d6de3e2adbf52820501
ryan-mckee <63581076+ryan-mckee@users.noreply.github.com>
2020-04-27 23:59:21 -0700

    Basic LAN server and client communication
    Added support for simple 2 way TCP communication over LAN

--- client.cpp
+++ client.cpp
@@ -1,19 +1,48 @@
 #include <iostream>
 #include <SFML/Network.hpp>
+#include <SFML/Graphics.hpp>
 
-int main()
+int main(int argc, char *argv[])
 {
   sf::TcpSocket socket;
-  sf::Socket::Status status = socket.connect("10.0.0.16", 53000);
+  //default ip adderss, enter a different one as first argument if necessary, or recompile. second argument is port. otherwise is just fine running with no arguments
+  std::string ipAddress = "10.0.0.206";
+  int port = 52000;
+  if (argc > 1)
+    ipAddress = argv[1];
+  if (argc > 2)
+    port = atoi(argv[2]);
+
+  sf::Socket::Status status = socket.connect(ipAddress, port);
   if (status != sf::Socket::Done)
   {
     // error...
   }
-  char data[100] = "Hello World";
 
-  // TCP socket:
-  if (socket.send(data, 100) != sf::Socket::Done)
+  //data must end in a \n, or the packet doesn't seem to send properly.
+  //It would instead be sent in the next packet containing a a newline
+  char dataOut[100] = "\n\n\n\n\n\n\n";
+  char dataIn[100];
+  std::size_t received;
+  //format of data - w a s d q i
+  do
   {
-    // error...
+    dataOut[0] = sf::Keyboard::isKeyPressed(sf::Keyboard::W);
+    dataOut[1] = sf::Keyboard::isKeyPressed(sf::Keyboard::A);
+    dataOut[2] = sf::Keyboard::isKeyPressed(sf::Keyboard::S);
+    dataOut[3] = sf::Keyboard::isKeyPressed(sf::Keyboard::D);
+    dataOut[4] = sf::Keyboard::isKeyPressed(sf::Keyboard::Q);
+    dataOut[5] = sf::Keyboard::isKeyPressed(sf::Keyboard::I);
+    //send input to server
+    if (socket.send(dataOut, 100) != sf::Socket::Done)
+    {
+      // error...
+    }
+    //receive info back from server
+    if (socket.receive(dataIn, 100, received) != sf::Socket::Done)
+    {
+      // error...
+    }
   }
+  while (dataOut[4] != 1);
 }
--- server.cpp
+++ server.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 #include <SFML/Network.hpp>
 
-int main()
+int main(int argc, char *argv[])
 {
+  //prints out the local and public ip addresses of the server.
+  //Currently only works on lan
   std::cout << sf::IpAddress::getLocalAddress() << std::endl;
-  std::cout << sf::IpAddress::getPublicAddress() << std::endl;
-  sf::TcpListener listener;
+  //std::cout << sf::IpAddress::getPublicAddress() << std::endl;
+  //default port is 52000, first argument is the modified port value
 
+  //is fine without arguments most of the time
+  int port = 52000;
+  sf::TcpListener listener;
+  if (argc > 1)
+    port = atoi(argv[1]);
   // bind the listener to a port
-  if (listener.listen(53000) != sf::Socket::Done)
+  if (listener.listen(port) != sf::Socket::Done)
   {
     // error...
   }
@@ -19,14 +26,27 @@ int main()
   {
     // error...
   }
-  char data[100];
-  std::size_t received;
 
-  // TCP socket:
-  if (client.receive(data, 100, received) != sf::Socket::Done)
+  char dataIn[100];
+  char dataOut[100] = "Hello Client\n";
+  std::size_t received;
+  do
   {
-    // error...
+    //receive input from the client
+    if (client.receive(dataIn, 100, received) != sf::Socket::Done)
+    {
+      // error...
+    }
+    //return info back to client
+    if (client.send(dataOut, 100) != sf::Socket::Done)
+    {
+      // error...
+    }
+
+    //print out keyboard inputs
+    for (int i = 0; i < 6; ++i)
+      std::cout << (char) (dataIn[i] + '0');
+    std::cout << std::endl;
   }
-  std::cout << "Received " << received << " bytes" << std::endl;
-  std::cout << data << std::endl;
+  while (dataIn[4] != 1);
 }
