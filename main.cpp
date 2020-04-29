3b262ce29aa20c6cded5cf65024d6267d632520f
Cole Harrington <harringtoncol17@gmail.com>
2020-04-29 11:24:10 -0700

    forgot main
    boom

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include "BasicZombie.h"
 #include <time.h>
 
+
 int main(int argc, char const* argv[]) {
     //Create Window
     srand(time(NULL));
@@ -15,31 +16,32 @@ int main(int argc, char const* argv[]) {
     window.setFramerateLimit(30);
 
     
-	PlayerManager playerManager;
+	
     Utility ut;
     Map map(1);
+    PlayerManager playerManager(&map);
     int i = rand() % 6 + 1;
     i = 7;
     printf("%d", i);
    
         BasicZombie* zombie1,*zombie2, *zombie3, *zombie4, *zombie5, *zombie6;
-        zombie1 = new BasicZombie();
+        zombie1 = new BasicZombie(playerManager.currentPlayer);
         if (i > 1)
-       zombie2 = new BasicZombie();
+       zombie2 = new BasicZombie(playerManager.currentPlayer);
     if (i > 2)
-         zombie3 = new BasicZombie();
+         zombie3 = new BasicZombie(playerManager.currentPlayer);
     if (i > 3)
-        zombie4 = new BasicZombie();
+        zombie4 = new BasicZombie(playerManager.currentPlayer);
     if (i > 4)
-        zombie5 = new BasicZombie();
+        zombie5 = new BasicZombie(playerManager.currentPlayer);
     if (i > 5)
-         zombie6 = new BasicZombie();
+         zombie6 = new BasicZombie(playerManager.currentPlayer);
 
 
 
 
     //Game Loop
-    while (window.isOpen()) {
+    while (window.isOpen() && playerManager.currentPlayer->alive()) {
         //Check if the window was closed
         sf::Event event;
         while (window.pollEvent(event)) {
@@ -48,15 +50,20 @@ int main(int argc, char const* argv[]) {
 
         //Render
         window.clear(sf::Color::Green);
-		
-		for (int i = 0; i < GameObject::GameObjects.size(); i++)
-		{
-            
-            std::cout << GameObject::GameObjects[i]->position.x << "  " << GameObject::GameObjects.size() << std::endl;
-			GameObject::GameObjects[i]->Update(window);
-		}
+      
+            for (int i = 0; i < GameObject::GameObjects.size(); i++)
+            {
+
+                std::cout << GameObject::GameObjects[i]->position.x << "  " << GameObject::GameObjects.size() << std::endl;
+                GameObject::GameObjects[i]->Update(window);
+            }
+
+       
       //  map.displayMap(window);
         window.display();
     }
+
+
+    std::cout << "can we write things here?" << endl;
     return 0;
 }
